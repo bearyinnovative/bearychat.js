b281cad43882c4c3b17b6d038a354974c61bbfb7
hbc <bcxxxxxx@gmail.com>
2017-02-16 17:15:27 +0800

    feat(api): validate parameters

--- src/_validate.js
+++ src/_validate.js
@@ -1,4 +1,36 @@
-function validate(path, payload) {
+const validation = require('./_validation.json');
+
+function getValidationSettings(method, path) {
+  let rv;
+
+  rv = validation[path];
+  if (!rv) return;
+
+  rv = rv[method]
+  if (!rv) return;
+
+  return rv;
+}
+
+function validate(method, path, payload) {
+  payload = payload || {};
+
+  const s = getValidationSettings(method, path);
+  if (!s) {
+    return 'unsupported api';
+  }
+
+  if (!s.authentication === false && !payload.token) {
+    return 'auth token required';
+  }
+
+  for (let p of Object.keys(s.parameters)) {
+    const ps = s.parameters[p];
+    if (ps.required && payload[p] === undefined) {
+      return `${p} required`;
+    }
+  }
+
   return;
 }
 
