dfb50baea2ad2a876a00250cbad88e2711b808c5
hbc <bcxxxxxx@gmail.com>
2017-03-11 11:04:39 +0800

    feat(api.message): add `GET /message.info` api

--- src/message.js
+++ src/message.js
@@ -2,10 +2,12 @@ import create from './message.create';
 import deleteMessage from './message.delete';
 import query from './message.query';
 import updateText from './message.update_text';
+import info from './message.info';
 
 export default {
   create,
   delete: deleteMessage,
   query,
   updateText,
+  info,
 };
